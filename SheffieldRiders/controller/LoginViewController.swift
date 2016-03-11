f18efcdf1f52d3ef5ccfdb6b6603a0335ed136c9
Tom Sugarex <Lordx@iMac.home>
2016-03-11 14:22:16 +0000

    Add login modal and sweet keyboard animations, Twitter style

---
+++ SheffieldRiders/controller/LoginViewController.swift
@@ -0,0 +1,57 @@
+//
+//  LoginViewController.swift
+//  SheffieldRiders
+//
+//  Created by Tom Sugarex on 11/03/2016.
+//  Copyright © 2016 Tom Sugarev. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var keyboardHeight: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        observeKeyboard()
+    }
+    
+    func observeKeyboard() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+
+    
+    @IBAction func dismissView(sender: UIButton) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification){
+        let info : NSDictionary = notification.userInfo!
+        let kbFrame: NSValue = info.objectForKey(UIKeyboardFrameEndUserInfoKey) as! NSValue
+        
+        let animationDuration: NSTimeInterval = (info.objectForKey(UIKeyboardAnimationDurationUserInfoKey)?.doubleValue)!
+        let keyboardFrame: CGRect = kbFrame .CGRectValue();
+        
+        let height: CGFloat = keyboardFrame.size.height
+        
+        self.keyboardHeight.constant = height
+        
+        UIView.animateWithDuration(animationDuration) { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+        
+    }
+
+    func keyboardWillHide(notification: NSNotification){
+        let info : NSDictionary = notification.userInfo!
+        let animationDuration: NSTimeInterval = (info.objectForKey(UIKeyboardAnimationDurationUserInfoKey)?.doubleValue)!
+        self.keyboardHeight.constant = 0
+        
+        UIView.animateWithDuration(animationDuration) { () -> Void in
+            self.view.layoutIfNeeded()
+        }
+    }
+}
