a4dc6115f7b8777a0457b8920889f8590488206d
Tom Sugarex <Lordx@iMac.home>
2016-03-11 19:25:00 +0000

    Add constants file for things like base url

---
+++ SheffieldRiders/resources/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  constants.swift
+//  SheffieldRiders
+//
+//  Created by Tom Sugarex on 11/03/2016.
+//  Copyright © 2016 Tom Sugarev. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    
+    static let apiBaseURL = "http://localhost:3000/"
+}
