ed488470473433241f617e0212b09fc474b39741
Tom Sugarex <Lordx@iMac.home>
2016-03-24 01:46:35 +0000

    Fixes

--- SheffieldRiders/controller/ProfileTableViewController.swift
+++ SheffieldRiders/controller/ProfileTableViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ProfileTableViewController: UITableViewController {
 
+    @IBOutlet weak var headerCell: UITableViewCell!
+    @IBOutlet weak var locationVisitedCell: UITableViewCell!
+    @IBOutlet weak var statisticsCell: UITableViewCell!
+    @IBOutlet weak var rideCell: UITableViewCell!
+    @IBOutlet weak var locationVisitedView: UIView!
+    @IBOutlet weak var stackview: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,21 +25,25 @@ class ProfileTableViewController: UITableViewController {
         navigationItem.rightBarButtonItem = DropDownMenu.sharedInstance.menuButton
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "showMenu", name: "showMenu", object: nil)
+    }
+    
+    
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         
+        switch indexPath.row {
+        case 0: return headerCell.frame.height
+        case 1: return stackview.frame.height
+        case 2: return statisticsCell.frame.height
+        case 3: return rideCell.frame.height
+        default: return 200
+        }
     }
     
     func showMenu() {
+        
+        tableView.setContentOffset(CGPointMake(0.0, -tableView.contentInset.top), animated:true)
+        
         DropDownMenu.sharedInstance.showMenu(self.view)
-    }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
     }
 
 
