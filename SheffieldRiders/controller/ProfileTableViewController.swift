ac6a37b4c26f3f1389dbafb9fe0b5a1b4c2629f9
Tom Sugarex <Lordx@iMac.home>
2016-03-24 00:39:46 +0000

    Make profile view a tableview

--- SheffieldRiders/controller/ProfileTableViewController.swift
+++ SheffieldRiders/controller/ProfileTableViewController.swift
@@ -1,17 +1,15 @@
 //
-//  ProfileViewController.swift
+//  ProfileTableViewController.swift
 //  SheffieldRiders
 //
-//  Created by Tom Sugarex on 23/03/2016.
+//  Created by Tom Sugarex on 24/03/2016.
 //  Copyright © 2016 Tom Sugarev. All rights reserved.
 //
 
 import UIKit
 
+class ProfileTableViewController: UITableViewController {
 
-class ProfileViewController: UIViewController {
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,5 +24,18 @@ class ProfileViewController: UIViewController {
     func showMenu() {
         DropDownMenu.sharedInstance.showMenu(self.view)
     }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+    
 
 }
