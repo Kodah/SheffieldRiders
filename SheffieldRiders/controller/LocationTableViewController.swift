ea1c73ed5022bb4de0c440bc6d95aa488ee560c0
Tom Sugarex <Lordx@iMac.home>
2016-03-28 17:46:32 +0100

    Implement location tableview

--- SheffieldRiders/controller/LocationTableViewController.swift
+++ SheffieldRiders/controller/LocationTableViewController.swift
@@ -11,45 +11,77 @@ import CoreData
 
 class LocationTableViewController: UITableViewController {
     
-    var locations = [NSManagedObject]()
+    var locationsArray = [Dictionary<String, NSObject>]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-        let appDelegate =
-        UIApplication.sharedApplication().delegate as! AppDelegate
+        navigationItem.title = "Locations"
         
-        let managedContext = appDelegate.managedObjectContext
+        navigationItem.leftBarButtonItem = DropDownMenu.sharedInstance.menuButton
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "showMenu", name: "showMenu", object: nil)
+        
+        loadJson()
 
-        let fetchRequest = NSFetchRequest(entityName: "Location")
-
-        do {
-            let results =
-            try managedContext.executeFetchRequest(fetchRequest)
-            locations = results as! [NSManagedObject]
-        } catch let error as NSError {
-            print("Could not fetch \(error), \(error.userInfo)")
-        }
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
-    // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return locationsArray.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("locationCell", forIndexPath: indexPath) as! LocationTableViewCell
+        
+        let location = locationsArray[indexPath.row]
+        
+        cell.titleLabel.text = location["name"] as? String
+        cell.disciplineLabel.text = location["discipline"] as? String
+        
+        
+        return cell
     }
 
-
+    func loadJson(){
+        
+        if let path = NSBundle.mainBundle().pathForResource("locations", ofType: "json")
+        {
+            if let jsonData = NSData(contentsOfFile: path)
+            {
+                do
+                {
+                    locationsArray =  try NSJSONSerialization.JSONObjectWithData(jsonData, options: []) as! [Dictionary<String, NSObject>]
+                    
+                }
+                catch
+                {
+                    print("error")
+                }
+            }
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if let identifier = segue.identifier {
+            if (identifier == "showLocation"){
+                
+                let viewController = segue.destinationViewController as! NewsViewController
+                
+                viewController.newsArticle = newsArray[selectedArticle]
+                
+                
+            }
+        }
+        
+    }
 
 }
