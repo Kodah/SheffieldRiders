b31a8b56ecf5862f4463d95169bf37e93de79d4b
Tom Sugarex <Lordx@iMac.home>
2016-04-15 23:45:38 +0100

    Nav bug

--- SheffieldRiders/controller/EventTypeTableViewController.swift
+++ SheffieldRiders/controller/EventTypeTableViewController.swift
@@ -15,14 +15,9 @@ class EventTypeTableViewController: UITableViewController {
         
         navigationItem.title = "Event Type"
 
-        
-
-    }
-
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
         navigationItem.leftBarButtonItem = DropDownMenu.sharedInstance.menuButton
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(self.showMenu), name: "showMenu", object: nil)
+
     }
     
     func showMenu() {
